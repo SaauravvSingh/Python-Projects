471fe1bee210971e7016d32f7c40237e15a75648
Saurav Singh <singhsaurav12945@gmail.com>
2026-08-18 23:33:06 +0530

    Add files via upload

--- PDF Locker/PDF_Locker.py
+++ PDF Locker/PDF_Locker.py
@@ -1,1 +1,13 @@
-.
+from pypdf import PdfReader, PdfWriter
+
+reader = PdfReader("Saurav_Singh_TCS_Certificate.pdf")
+
+writer = PdfWriter()
+writer.append(reader)
+
+writer.encrypt("54321")
+
+with open("protected.pdf", "wb") as file:
+    writer.write(file)
+
+print("PDF password protected successfully!")
